64f3ece54eb5c9b0588586472f408bfb4c30dc50
Rakesh <rakeshrp@yahoo.com>
2023-04-07 16:59:39 +1000

    CircleCi configured

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,10 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 120 * 1000,
+  testMatch: [
+//Test case that needs to be executed
+  ],
+  timeout: 30 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
@@ -25,14 +28,20 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? [["junit", {
+    outputFile: "result.xml"
+  }]] : [['json', {
+    outputFile: "report.json"
+  }],["html", {
+    open:"on-failure"
+  }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: process.env.CI ? true : false,
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -40,6 +49,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: "on",
+    screenshot: "on"
   },
 
   /* Configure projects for major browsers */
